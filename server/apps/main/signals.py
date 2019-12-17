7647ba272c661b8951f851070e40f788f6a100bd
Kerin Cosford <cosford@gmail.com>
2019-12-17 12:32:26 +0000

    Retain kebab case header names

--- server/apps/main/signals.py
+++ server/apps/main/signals.py
@@ -5,7 +5,7 @@ from django_structlog.signals import bind_extra_request_metadata
 
 @receiver(bind_extra_request_metadata)
 def bind_cf_request_ids(request, logger, **kwargs):
-    logger.bind(
-        x_b3_traceid=request.headers.get('X-B3-TraceId'),
-        x_b3_spanid=request.headers.get('X-B3-SpanId')
-    )
+    logger.bind(**{
+        "X-B3-TraceId": request.headers.get('X-B3-TraceId'),
+        "X-B3-SpanId": request.headers.get('X-B3-SpanId')
+    })
