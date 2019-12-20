a55ada95f26d5e87d1a1e67eedeca79741c14c9d
Ravi Kotecha <kotecha.ravi@gmail.com>
2019-12-20 12:09:09 +0000

    Add support for Hawk Authentication using TokenAuth model
    The Hawk key is the value of the token created in the django admin for a given
    user. If a user is not a system user then they should not be assigned a token
    
    Co-Authored-By: Kerin Cosford <kerin@users.noreply.github.com>

--- server/apps/main/admin.py
+++ server/apps/main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from rest_framework.authtoken.admin import TokenAdmin
 
 from server.apps.main.models import Consent, LegalBasis
 
 # Register your models here.
 admin.site.register(LegalBasis)
 admin.site.register(Consent)
+
+# Recommended by drf docs: https://www.django-rest-framework.org/api-guide/authentication/#with-django-admin
+TokenAdmin.raw_id_fields = ['user']
