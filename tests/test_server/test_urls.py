c37b0d8ce3707286e02da6b15c1f11de73c41133
Kerin Cosford <cosford@gmail.com>
2020-01-08 15:46:49 +0000

    Remove humans.txt test

--- tests/test_server/test_urls.py
+++ tests/test_server/test_urls.py
@@ -44,11 +44,3 @@ def test_robots_txt(client):
 
     assert response.status_code == 200
     assert response.get("Content-Type") == "text/plain"
-
-
-def test_humans_txt(client):
-    """This test ensures that `humans.txt` is accessible."""
-    response = client.get("/humans.txt")
-
-    assert response.status_code == 200
-    assert response.get("Content-Type") == "text/plain"
