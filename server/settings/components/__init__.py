459a2eb89ca7379b9ca4f186d8369cd4fede9a3c
Kerin Cosford <cosford@gmail.com>
2020-01-14 16:54:29 +0000

    Replace python-decouple with django-environ

--- server/settings/components/__init__.py
+++ server/settings/components/__init__.py
@@ -1,11 +1,13 @@
+import os
 from pathlib import PurePath
 
-from decouple import AutoConfig
+import environ
 
 # Build paths inside the project like this: BASE_DIR.joinpath('some')
 # `pathlib` is better than writing: dirname(dirname(dirname(__file__)))
 BASE_DIR = PurePath(__file__).parent.parent.parent.parent
 
 # Loading `.env` files
-# See docs: https://gitlab.com/mkleehammer/autoconfig
-config = AutoConfig(search_path=BASE_DIR.joinpath("config"))
+# See docs: https://github.com/joke2k/django-environ
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, "config", ".env"))
