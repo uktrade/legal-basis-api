f55bfb7302d1a6ca37a2db47310bc27be2f5ed1c
Ravi Kotecha <kotecha.ravi@gmail.com>
2020-02-25 16:33:44 +0000

    Change waitress-server threads from 1 to 4
    With the possibility to override using the `WEB_CONCURRENCY` environment variable

--- bin/run.sh
+++ bin/run.sh
@@ -16,4 +16,4 @@ export DJANGO_ENV
 # Run python specific scripts:
 # Running migrations in startup script might not be the best option, see:
 # docs/pages/template/production-checklist.rst
-waitress-serve --listen=0.0.0.0:$PORT server.wsgi:application
+waitress-serve --listen=0.0.0.0:$PORT --threads=${WEB_CONCURRENCY:-4} server.wsgi:application
