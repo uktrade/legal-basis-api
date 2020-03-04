7e65bfb7c42671cf3507baf27ad8f7d13644da67
Ravi Kotecha <kotecha.ravi@gmail.com>
2020-03-04 10:48:52 +0000

    Disallow anonymous read access to API
    Change DEFAULT_PERMISSION_CLASSES to `DjangoModelPermissions` from
    `DjangoModelPermissionsOrAnonReadOnly`

--- server/settings/components/drf.py
+++ server/settings/components/drf.py
@@ -10,7 +10,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        "rest_framework.permissions.DjangoModelPermissions",
     ],
     "ORDERING_PARAM": "sortby",
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
