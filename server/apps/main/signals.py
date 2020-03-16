1e45769f7a9e8cc1f3fc4961d0492ae11ebfdaae
Kerin Cosford <cosford@gmail.com>
2020-03-16 14:44:46 +0000

    At extra request info to request logs
    As per DIT manual howto

--- server/apps/main/signals.py
+++ server/apps/main/signals.py
@@ -1,12 +1,19 @@
 from django.dispatch import receiver
+from django.utils import timezone
 from django_structlog.signals import bind_extra_request_metadata
 
 
 @receiver(bind_extra_request_metadata)
-def bind_cf_request_ids(request, logger, **kwargs):
+def bind_extra_log_data(request, logger, **kwargs):
     logger.bind(
         b3=request.headers.get("B3"),
         x_b3_traceid=request.headers.get("X-B3-TraceId"),
         x_b3_spanid=request.headers.get("X-B3-SpanId"),
         request_id=request.headers.get("X-B3-TraceId"),
+        sso_user_id=request.user.username,
+        local_user_id=request.user.id,
+        path=request.path,
+        host=request.get_host(),
+        service="Consent API",
+        request_time=timezone.now().isoformat(),
     )
