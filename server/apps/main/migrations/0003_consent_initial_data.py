fdd04429facd4643c781919a20c7de42ff6af6a8
Kerin Cosford <cosford@gmail.com>
2020-03-25 11:44:40 +0000

    use hardcoded consent_types list in initial data migration
    settings.CONSENT_TYPES could change, so safer to hardcode within the migration

--- server/apps/main/migrations/0003_consent_initial_data.py
+++ server/apps/main/migrations/0003_consent_initial_data.py
@@ -1,22 +1,23 @@
-
-from django.conf import settings
 from django.db import migrations
 
 from server.apps.main.models import Consent
 
+# Also present in settings.common, but hardcoded here
+# so that migrations don't break if the settings are changed
+CONSENT_TYPES = ("email_marketing", "phone_marketing")
+
 
 class Migration(migrations.Migration):
-
     def create_consent(apps, schema_editor):
-        for name in settings.CONSENT_TYPES:
+        for name in CONSENT_TYPES:
             Consent.objects.get_or_create(name=name)
 
     def delete_consent(apps, schema_editor):
-        for name in settings.CONSENT_TYPES:
+        for name in CONSENT_TYPES:
             Consent.objects.get(name=name).delete()
 
     dependencies = [
-        ('main', '0002_auto_20200218_1028'),
+        ("main", "0002_auto_20200218_1028"),
     ]
 
     operations = [
