05e448eb85537b9d58d57489c58d73f5d80481cc
Harel Malka <harel@harelmalka.com>
2020-09-28 15:35:12 +0100

    cleanup and flake8

--- server/apps/poller/management/commands/poll_adobe_campaign.py
+++ server/apps/poller/management/commands/poll_adobe_campaign.py
@@ -1,5 +1,4 @@
-import pytz
-import logging 
+import logging
 from datetime import datetime, timedelta
 from time import sleep
 
@@ -13,8 +12,8 @@ from django.utils.functional import cached_property
 from django_tqdm import BaseCommand
 
 from server.apps.main.models import KEY_TYPE, Commit, Consent, LegalBasis
-from server.apps.poller.api_client.adobe import AdobeClient 
-from server.apps.poller.models import AdobeCampaign 
+from server.apps.poller.api_client.adobe import AdobeClient
+from server.apps.poller.models import AdobeCampaign
 
 queryset = LegalBasis.objects.filter(current=True)
 logger = logging.getLogger(__name__)
@@ -23,11 +22,11 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = """
     Start polling for Adobe campaign subscriptions and unsubscriptions
-    The campaigns to track should be set up in the AdobeCampaign model. 
+    The campaigns to track should be set up in the AdobeCampaign model.
 
-    The process will: 
-    1. For each adobe current subscription, verify that consent is still given, and unsubscribe if not. 
-    2. Kick off the Adobe workflow to generate unsubscription details, and for each adobe unsubscription, remove consent. 
+    The process will:
+    1. For each adobe current subscription, verify that consent is still given, and unsubscribe if not.
+    2. Kick off the Adobe workflow to generate unsubscription details, and for each adobe unsubscription, remove consent.
 
     e.g. ./manage.py poll_adobe_campaign
     """
@@ -87,14 +86,14 @@ class Command(BaseCommand):
                 obj.consents.remove(self.email_consent)
 
             self._send_action(obj)
- 
+
     def has_consent(self, email_address):
         """
         Returns True if this email address has a current consent, False otherwise
         """
         if email_address:
             try:
-                lb: LegalBasis = queryset.get(email=email_address)
+                queryset.get(email=email_address)
                 return True
             except LegalBasis.DoesNotExist:
                 pass
@@ -103,51 +102,58 @@ class Command(BaseCommand):
     def get_service_campaigns(self):
         return AdobeCampaign.objects.filter(active=True)
 
+    def validate_campaign_subscribers(self, campaign, client):
+        self.write(f"Processing: {campaign.name}")
+        subscribers = client.subscriptions(campaign.pkey)
+        total = next(subscribers)
+        unsubscribed = 0
+        with self.tqdm(total=total) as progress_bar:
+            for subscription in subscribers:
+                profile = subscription.get('subscriber', {})
+                if profile:
+                    email_address = profile.get('email')
+                    if self.has_consent(email_address) is False:
+                        client.unsubscribe(subscription.get('PKey'))
+                        unsubscribed += 1
+                progress_bar.update(1)
+        campaign.last_fetched_at = timezone.now()
+        campaign.save()
+        return unsubscribed
+
+    def process_unsubscribe_events(self, campaign, client):
+        self.write(f"Processing unsubscriptions for: {campaign.name}")
+        unsubscribers = client.get_unsubscribers()
+        unsubscription_events = unsubscribers.get('content', [])
+        total = unsubscribers.get('count', {}).get('value')
+        consents_removed = 0
+        with self.tqdm(total=total) as progress_bar:
+            self.write(f"Processing {total} unsubscription events")
+            for unsub in unsubscription_events:
+                service = unsub.get('service')
+                if service == campaign.name:  # see about changing to service pkey
+                    email = unsub.get('email')
+                    self.update_consent(
+                        email_address=email,
+                        meta={
+                            "emt_id": unsub.get('EMT_ID'),
+                            "action_date": unsub.get('actionDate')
+                        },
+                        consent = False)
+                    self.write(f"Update sunsub: {email}  {unsub.get('EMT_ID')}")
+                    client.delete_unsubscribers(unsub.get('PKey'))
+                    consents_removed += 1
+                progress_bar.update(1)
+        return consents_removed, bool(unsubscription_events)
+
     def run(self, *args, **options) -> None:
         client = AdobeClient()
-        unsubscribed = 0
-        consents_removed = 0
+
         service_campaigns = self.get_service_campaigns()
         for campaign in service_campaigns:
-            self.write(f"Processing: {campaign.name}")
-            subscribers = client.subscriptions(campaign.pkey)
-            total = next(subscribers)
-            
-            with self.tqdm(total=total) as progress_bar:
-                for subscription in subscribers:
-                    profile = subscription.get('subscriber', {})
-                    if profile:
-                        email_address = profile.get('email')
-                        if self.has_consent(email_address) is False:
-                            client.unsubscribe(subscription.get('PKey'))
-                            unsubscribed += 1
-                    progress_bar.update(1)  
-            campaign.last_fetched_at = timezone.now()
-            campaign.save()
-
-            # check for unsubscriptions 
-            self.write(f"Processing unsubscriptions for: {campaign.name}")
-            unsubscribers = client.get_unsubscribers()
-            unsubscription_events = unsubscribers.get('content', [])
-            total = unsubscribers.get('count', {}).get('value')
-            with self.tqdm(total=total) as progress_bar:
-                self.write(f"Processing {total} unsubscription events")
-                for unsub in unsubscription_events:
-                    service = unsub.get('service')
-                    if service == campaign.name:  # see about changing to service pkey
-                        email = unsub.get('email')
-                        self.update_consent(
-                            email_address=email, 
-                            meta={
-                                "emt_id": unsub.get('EMT_ID'),
-                                "action_date": unsub.get('actionDate')
-                            }, 
-                            consent = False)
-                        self.write(f"Update sunsub: {email}  {unsub.get('EMT_ID')}")
-                        client.delete_unsubscribers(unsub.get('PKey'))
-                        consents_removed += 1
-                    progress_bar.update(1)
-            if unsubscription_events and settings.ADOBE_STAGING_WORKFLOW:
+            # Validate campaign subscribers
+            unsubscribed = self.validate_campaign_subscribers(campaign, client)
+            consents_removed, has_unsubs = self.process_unsubscribe_events(campaign, client)
+            if has_unsubs and settings.ADOBE_STAGING_WORKFLOW:
                 self.write("Initiating cleanup workflow")
                 client.start_workflow(settings.ADOBE_STAGING_WORKFLOW)
         self.write(f"Adobe cycle complete. Unsubscribed={unsubscribed}, Consents removed={consents_removed}")
